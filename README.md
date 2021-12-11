26aff732e15e4d28d500044bbad67d5514fa27ef
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-12-11 13:17:59 -0500

    add website files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# rp-portfolio
-Ryan Popp portfolio
+# Ryan Popp's Portfolio
+
+## Developing
+
+1. To serve the website locally, from a terminal, run:
+
+```javascript
+npx serve
+```
+
+2. Open a browser and navigate to [http://localhost:3000.com](http://localhost:3000.com)
+3. Edit the code
+4. Refresh your browser
+
+## Learning
+
+### HTML
+
+### CSS
+
+### Javascript
+
+### React
+
+- https://reactjs.org/
---
+++ index.js
@@ -0,0 +1,32 @@
+// TODO replace urls with Ryan's
+const urls = {
+  GitHub: "https://github.com/",
+  Twitter: "https://twitter.com/home",
+  Twitch: "https://www.twitch.tv/",
+  Mail: "myemail@gmail.com",
+  Instagram: "https://www.instagram.com/ryanpopp18/",
+};
+
+function goTo(url) {
+  window.open(url, "_blank");
+}
+
+function goToGitHub() {
+  goTo(urls.GitHub);
+}
+
+function goToTwitter() {
+  goTo(urls.Twitter);
+}
+
+function goToTwitch() {
+  goTo(urls.Twitch);
+}
+
+function goToInstagram() {
+  goTo(urls.Instagram);
+}
+
+function mailTo() {
+  goTo(`mailto:${urls.Mail}`);
+}
